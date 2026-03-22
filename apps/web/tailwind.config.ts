5f049e7b69390c16cfbdd0accfac1b09f1bd1af5
erzan <do.earljan@gmail.com>
2026-03-22 09:20:48 +0800

    updated tailwindcss added new color palletes

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: {
+          DEFAULT: '#faedcd',
+          dark: '#5f553d',
+        },
+      },
+    },
   },
   plugins: [],
 }
